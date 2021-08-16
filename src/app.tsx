49a9231ce6ff91c232e37334fcffa553c2b8eb32
arthur <arthur.escriou@gmail.com>
2021-08-16 13:43:31 +0200

    move controller

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import { v4 as uuid } from 'uuid'
 import './app.css'
-import { State, INSTRUCTION } from './engine'
+import { State } from './engine'
+import danceController from './controllers/dance'
 import { BASIC_SHIP } from './engine/config'
-import { Ship, RadarResult } from './engine/ship'
 import { Renderer } from '@/renderer'
 import { Engine } from '@/engine'
 
@@ -20,33 +20,7 @@ const defaultState: State = {
   bullets: [],
 }
 
-const controllers = defaultState.ships.map(ship => {
-  let num = Math.random() * 100
-  let cptDist = 20 + Math.random() * 100
-  let cptTurn = 20 + Math.random() * 20
-  const shipId = ship.id
-  const getInstruction = (ship: Ship, radar: Array<RadarResult>) => {
-    if (num > 0) {
-      num--
-      return INSTRUCTION.TURN_RIGHT
-    }
-    if (cptDist <= 0 && cptTurn <= 0) {
-      cptDist = 20 + Math.random() * 100
-      cptTurn = 20 + Math.random() * 20
-    }
-
-    if (cptDist < 0 && cptTurn > 0) {
-      if (ship.position.speed) {
-        return INSTRUCTION.BACK_THRUST
-      }
-      cptTurn--
-      return INSTRUCTION.TURN_RIGHT
-    }
-    cptDist--
-    return ship.position.speed < 11 ? INSTRUCTION.THRUST : INSTRUCTION.IDLE
-  }
-  return { shipId, getInstruction }
-})
+const controllers = defaultState.ships.map(danceController)
 
 const App = () => {
   const [engine] = useState(new Engine(defaultState, controllers))
