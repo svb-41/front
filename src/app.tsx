8978f7c4591a8156e9b1e67d06f6708f26e7b2cb
Guillaume Hivert <hivert.is.coming@gmail.com>
2021-08-25 17:17:13 +0200

    :truck: Move HUD to components

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,13 @@
 import { useState, Fragment } from 'react'
 import { Renderer } from '@/renderer'
 import defaultEngine from '@/gameSetups/default'
-import { HUD } from '@/hud'
+import * as HUD from '@/components/hud'
 
 const App = () => {
-  const [engine] = useState(defaultEngine)
   return (
     <Fragment>
-      <HUD />
       <Renderer engine={engine} />
+      <HUD.Render state={screen} onClick={onClick} />
     </Fragment>
   )
 }
--- src/hud/index.tsx
+++
@@ -1,5 +0,0 @@
-import styles from './hud.module.css'
-
-export const HUD = () => {
-  return <div className={styles.hud}>HUD</div>
-}
