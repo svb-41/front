52d25f78b1a3e4d8160e14a7f03a8ed9b157517f
arthur.escriou <arthur.escriou@gmail.com>
2021-09-28 00:41:21 +0200

    :fire: remove netlifyfucntion

--- functions/compile/main.js
+++
@@ -1,9 +0,0 @@
-const babel = require('@babel/core')
-
-exports.handler = async function (event, context) {
-  babel.transformSync('code', optionsObject)
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello World' }),
-  }
-}
