43af3149806fc0de01c66511ed7cbc163a297e17
arthur.escriou <arthur.escriou@gmail.com>
2021-09-27 23:50:36 +0200

    add env

---
+++ src/envs/index.js
@@ -0,0 +1,6 @@
+const URL =
+  process.env.NODE_ENV === 'development'
+    ? 'https://m01wke3al4.execute-api.eu-west-3.amazonaws.com'
+    : 'https://m01wke3al4.execute-api.eu-west-3.amazonaws.com'
+
+export default { URL }
