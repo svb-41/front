c50e3d46d4873718ff6158a4be001e9a2d901382
arthur.escriou <arthur.escriou@gmail.com>
2022-01-28 17:42:59 +0100

    add routes

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from '@/pages/home'
+import Ia from '@/pages/ia'
 import Ships from '@/pages/ships'
+import Missions from '@/pages/missions'
+import Onboarding from '@/pages/onboarding'
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Home />} />
+    <Route path="ia" element={<Ia />} />
     <Route path="ships" element={<Ships />} />
+    <Route path="missions" element={<Missions />} />
+    <Route path="onboarding" element={<Onboarding />} />
   </Routes>
 )
 
