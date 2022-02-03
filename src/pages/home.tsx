64d04dae91e85a490d2b1beb09a0644fa3065106
arthur.escriou <arthur.escriou@gmail.com>
2022-02-03 19:03:33 +0100

    :lipstick: add home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,38 @@
-const Home = () => <>home</>
+import * as HUD from '@/components/hud'
+import styles from './Pages.module.css'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <>
+      <HUD.HUD title="SVB 41" back="/" button="Connect" />
+      <HUD.Container>
+        <div className={styles.container}>
+          <div className={styles.title}>Tableau de bord</div>
+          <div className={styles.col}>
+            <Link to="/missions">
+              <div className={styles.mainLink}>Missions</div>
+            </Link>
+            <Link to="/ia">
+              <div className={styles.mainLink}>IA</div>
+            </Link>
+          </div>
+          <Link to="/ships">
+            <div className={styles.mainLink}>Ships</div>
+          </Link>
+
+          <div className={styles.col}>
+            <Link to="/training">
+              <div className={styles.secondaryLink}>Training</div>
+            </Link>
+            <Link to="/sandbox">
+              <div className={styles.secondaryLink}>Sandbox</div>
+            </Link>
+          </div>
+        </div>
+      </HUD.Container>
+    </>
+  )
+}
 
 export default Home
