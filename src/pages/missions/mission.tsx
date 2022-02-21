387124b03a5b5ded449065c5a242c1851b66deb1
arthur.escriou <arthur.escriou@gmail.com>
2022-02-21 15:32:25 +0100

    :construction: launch game in mission

--- src/pages/missions/mission.tsx
+++ src/pages/missions/mission.tsx
@@ -1,16 +1,26 @@
+import { useEffect, useRef, useState } from 'react'
+import { Engine } from '@/engine'
+import { Renderer } from '@/renderer'
 import * as HUD from '@/components/hud'
 import { useLocation } from 'react-router-dom'
+import defaultEngine from '@/game-setups/default'
+import styles from './Missions.module.css'
 
 const Mission = () => {
   const location = useLocation()
   const search = location.pathname.split('/')
+  const [engine, setEngine] = useState<Engine>()
+
   const id = search[search.length - 1]
+
+  useEffect(() => {
+    defaultEngine().then(setEngine)
+  }, [])
+
   return (
     <>
       <HUD.HUD title="Missions" back="/missions" />
-      <HUD.Container>
-        <>mission {id}</>
-      </HUD.Container>
+      {engine ? <Renderer engine={engine} /> : <></>}
     </>
   )
 }
