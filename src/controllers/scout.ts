1228cec5655296602790748f8cf3f2ff25d855bb
arthur.escriou <arthur.escriou@gmail.com>
2022-03-11 17:19:48 +0100

    :bug: fix mission 5

--- src/controllers/scout.ts
+++ src/controllers/scout.ts
@@ -1,20 +1,19 @@
 import * as svb from '@svb-41/core'
-const { dist2 } = svb.helpers
 
 type Data = { initialDir?: number }
 type ControllerArgs = svb.controller.ControllerArgs<Data>
 
 export const initialData = {}
-export default ({ stats, memory, comm, radar, ship }: ControllerArgs) => {
-  if (!memory.initialDir) memory.initialDir = stats.position.direction
-  if (stats.position.speed < 0.08)
-    return ship.thrust(0.08 - stats.position.speed)
+export default ({ stats, comm, radar, ship }: ControllerArgs) => {
   if (radar.length > 0) {
     const enemies = radar
       .filter(res => res.team !== stats.team && !res.destroyed)
       .map(res => res.position)
     if (enemies.length > 0) comm.sendMessage(enemies)
   }
-  if (memory.initialDir - stats.position.direction === 0) return ship.idle()
-  return ship.turn(memory.initialDir - stats.position.direction)
+  if (stats.position.speed < 0.1) return ship.thrust(0.1 - stats.position.speed)
+  if (stats.position.speed - 0.1 >= 0.01)
+    return ship.thrust(stats.position.speed - 0.1)
+
+  return ship.idle()
 }
