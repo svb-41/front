a0ca3f95d7504d4689779002861ee2a8994a6168
arthur.escriou <arthur.escriou@gmail.com>
2022-03-11 15:14:52 +0100

    update missions

--- src/controllers/scout.ts
+++ src/controllers/scout.ts
@@ -7,25 +7,13 @@ type ControllerArgs = svb.controller.ControllerArgs<Data>
 export const initialData = {}
 export default ({ stats, memory, comm, radar, ship }: ControllerArgs) => {
   if (!memory.initialDir) memory.initialDir = stats.position.direction
-  if (stats.position.speed < 0.08) return ship.thrust()
+  if (stats.position.speed < 0.08)
+    return ship.thrust(0.08 - stats.position.speed)
   if (radar.length > 0) {
     const enemies = radar
       .filter(res => res.team !== stats.team && !res.destroyed)
       .map(res => res.position)
     if (enemies.length > 0) comm.sendMessage(enemies)
-    const importantTarget = radar
-      .filter(res => res.team !== stats.team && !res.destroyed)
-      .find(enemy => enemy.size === 16)
-    if (importantTarget) {
-      const targetDist = dist2(stats.position, importantTarget.position)
-      return svb.helpers.aim({
-        ship,
-        source: stats.position,
-        target: importantTarget!.position,
-        threshold: 4 / Math.sqrt(targetDist),
-        delay: Math.sqrt(targetDist) / stats.weapons[0]?.bullet.position.speed,
-      })
-    }
   }
   if (memory.initialDir - stats.position.direction === 0) return ship.idle()
   return ship.turn(memory.initialDir - stats.position.direction)
