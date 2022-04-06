f558fe42ce6337658efd54ddf8d67ed9e85f5b9e
Guillaume Hivert <hivert.is.coming@gmail.com>
2022-04-06 17:36:33 +0200

    Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import * as Redux from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
 import App from './app'
 import { store } from '@/store'
 import reportWebVitals from './reportWebVitals'
 import './index.css'
 import './remark.scss'
 import './atom-one-dark-theme.css'
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY,
+  integrations: [new BrowserTracing()],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+})
 
 ReactDOM.render(
   <React.StrictMode>
