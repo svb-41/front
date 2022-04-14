520a4c52d96c258ed4ccdd97821acebc65be6a3d
arthur.escriou <arthur.escriou@gmail.com>
2022-04-14 21:33:32 +0200

    add get AI

--- src/services/compile.ts
+++ src/services/compile.ts
@@ -7,3 +7,19 @@ export const compile = async (params: Compile): Promise<string> => {
   const res = await fetch(`${URL}/compile`, { method: 'POST', body })
   return await res.text()
 }
+
+export type AIparams = { uid: string; id: string; token?: string }
+export const getAI = async (params: AIparams) => {
+  const { uid, id, token } = params
+  const queryStringParameters = [
+    ['uid', uid],
+    ['id', id],
+    ['decompiled', token !== undefined],
+  ]
+    .map(a => a.join('='))
+    .join('&')
+  const res = await fetch(`${URL}/ai?${queryStringParameters}`, {
+    method: 'GET',
+  })
+  return await res.json()
+}
