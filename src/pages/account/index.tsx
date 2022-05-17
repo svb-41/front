454345af62118bd7b2427d7e1a774b889d997e31
arthur.escriou <arthur.escriou@gmail.com>
2022-05-18 01:29:30 +0200

    :spakles: create inventory service

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+import { getInventory } from '@/services/inventory'
 import { useSelector } from '@/store/hooks'
 import * as selectors from '@/store/selectors'
 import { Login } from '../login'
@@ -5,6 +7,10 @@ import { Details } from './details'
 
 export const Account = () => {
   const user = useSelector(selectors.userData)
+  useEffect(() => {
+    if (user.user?.accessToken)
+      getInventory({ token: user.user.accessToken }).then(console.log)
+  })
   if (!user.user) return <Login />
   return <Details />
 }
