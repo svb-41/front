234bfc1a0c38071b69a06bccbcf1c229182b8bb0
Guillaume Hivert <hivert.is.coming@gmail.com>
2022-05-24 00:02:26 +0200

    Add Desktop allegory

--- src/components/movable.tsx
+++ src/components/movable.tsx
@@ -1,82 +1,117 @@
 import { FC, useState, useEffect, useRef } from 'react'
-import { Column, Row } from '@/components/flex'
+import * as Flex from '@/components/flex'
 
-const usePosition = () => {
-  const [position, setPosition] = useState({ bottom: 24, right: 24 })
+const usePosition = (maxTop?: number) => {
+  const highTop = maxTop ?? 40
+  const [position, setPosition] = useState({ top: highTop + 24, left: 24 })
   const popupRef = useRef<any>(null)
   const divRef = useRef<any>(null)
   const values = useRef<any>(null)
+  const mouseup = () => (popupRef.current = null)
   useEffect(() => {
-    const resizer = () =>
-      (values.current = divRef.current?.getBoundingClientRect?.() ?? null)
-    const fun = (event: MouseEvent) => {
-      if (!values.current)
-        if (divRef.current) {
-          values.current = divRef.current.getBoundingClientRect()
-        } else {
-          return
-        }
-
-      if (popupRef.current) {
-        const x_ = popupRef.current.x - event.clientX
-        const y_ = popupRef.current.y - event.clientY
-        const right_ = x_ + popupRef.current.initialPosition.right
-        const bottom_ = y_ + popupRef.current.initialPosition.bottom
-        const bottom = Math.min(
-          Math.max(0, bottom_),
-          window.innerHeight - values.current!.height
-        )
-        const right = Math.min(
-          Math.max(0, right_),
-          window.innerWidth - values.current!.width
-        )
-        setPosition({ bottom, right })
+    const resize = () => {
+      values.current = divRef.current?.getBoundingClientRect?.() ?? null
+      if (values.current) {
+        setPosition(position => {
+          const minLeft = window.innerWidth - values.current.width
+          const left = Math.min(position.left, minLeft)
+          const minTop = window.innerHeight - values.current.height
+          const top = Math.min(position.top, minTop)
+          return { left, top }
+        })
       }
     }
-    document.addEventListener('resize', resizer)
-    document.addEventListener('mousemove', fun)
-    return () => {
-      document.removeEventListener('mousemove', fun)
-      document.removeEventListener('resize', resizer)
+    const mousemove = (event: MouseEvent) => {
+      if (!values.current)
+        if (!divRef.current) return
+        else values.current = divRef.current.getBoundingClientRect()
+      if (popupRef.current) {
+        const left_ = event.clientX - popupRef.current.originX
+        const bottom_ = event.clientY - popupRef.current.originY
+        const minTop = window.innerHeight - values.current!.height
+        const minLeft = window.innerWidth - values.current!.width
+        const top = Math.min(Math.max(highTop, bottom_), minTop)
+        const left = Math.min(Math.max(0, left_), minLeft)
+        setPosition({ top, left })
+      }
     }
-  }, [])
+    window.addEventListener('resize', resize)
+    document.addEventListener('mousemove', mousemove)
+    document.addEventListener('mouseup', mouseup)
+    return () => {
+      window.removeEventListener('resize', resize)
+      document.removeEventListener('mousemove', mousemove)
+      document.removeEventListener('mouseup', mouseup)
+    }
+  }, [maxTop])
   const onMouseDown = (event: any) => {
     popupRef.current = {
       x: event.clientX,
       y: event.clientY,
-      initialPosition: position,
+      originX: event.clientX - position.left,
+      originY: event.clientY - position.top,
     }
   }
-  const onMouseUp = () => (popupRef.current = null)
-  return { position, onMouseDown, onMouseUp, ref: divRef }
+  return { position, onMouseDown, ref: divRef }
 }
 
-export type Props = { onClose: () => void }
-export const Movable: FC<Props> = ({ children, onClose }) => {
-  const position = usePosition()
+export type Props = {
+  title?: string
+  zIndex?: number
+  onClose: () => void
+  minWidth?: number | string
+  minHeight?: number | string
+  maxTop?: number
+  padding?: Flex.Size
+  onMouseDown?: () => void
+}
+export const Movable: FC<Props> = props => {
+  const position = usePosition(props.maxTop)
+  const zIndex = props.zIndex ?? 1e10
+  const style: any = { position: 'fixed', ...position.position, zIndex }
   return (
-    <div
-      ref={position.ref}
-      style={{ position: 'fixed', ...position.position, zIndex: 1e10 }}
-    >
-      <Column
+    <div ref={position.ref} style={style} onMouseDown={props.onMouseDown}>
+      <Flex.Column
+        minWidth={props.minWidth}
+        minHeight={props.minHeight}
         style={{
           border: '2px solid var(--fff)',
           overflow: 'hidden',
           maxHeight: '90vh',
         }}
       >
-        <div onMouseDown={position.onMouseDown} onMouseUp={position.onMouseUp}>
-          <Row background="var(--fff)" justify="flex-end" padding="s">
-            <button onClick={onClose} style={{ fontSize: 30 }}>
+        <div onMouseDown={position.onMouseDown} style={{ userSelect: 'none' }}>
+          <Flex.Row
+            align="center"
+            background="var(--fff)"
+            justify={props.title ? 'space-between' : 'flex-end'}
+            padding="s"
+          >
+            <div
+              style={{
+                maxWidth: 200,
+                overflow: 'hidden',
+                whiteSpace: 'nowrap',
+                textOverflow: 'ellipsis',
+                paddingLeft: 'var(--s)',
+              }}
+            >
+              {props.title}
+            </div>
+            <button onClick={props.onClose} style={{ fontSize: 30 }}>
               x
             </button>
-          </Row>
+          </Flex.Row>
         </div>
-        <Column flex={1} style={{ overflow: 'auto' }}>
-          {children}
-        </Column>
-      </Column>
+        <Flex.Column
+          overflow="auto"
+          flex={1}
+          padding={props.padding}
+          background="var(--eee)"
+        >
+          {props.children}
+        </Flex.Column>
+      </Flex.Column>
     </div>
   )
 }
--- src/pages/tutorial/index.tsx
+++ src/pages/tutorial/index.tsx
@@ -1,14 +1,178 @@
-import { useState } from 'react'
+import { useState, useRef, useLayoutEffect } from 'react'
+import { Column, Row } from '@/components/flex'
+import { Movable } from '@/components/movable'
 import { Introduction } from './introduction'
-
+import * as uuid from 'uuid'
+import styles from './tutorial.module.css'
 export type Page = 'introduction' | 'start'
 
+const RenderMenu = (props: AppBarProps) => {
+  const top = `calc(${props.appHeight ?? 40}px + var(--s))`
+  return (
+    <Column
+      background="var(--fff)"
+      padding="s"
+      className={styles.unrolledMenu}
+      style={{ top }}
+    >
+      <div className={styles.unrolledMenuItem} onClick={props.onClick}>
+        Open new test…
+      </div>
+      <div className={styles.unrolledMenuItem} onClick={props.onClick}>
+        Open new test…
+      </div>
+      <div className={styles.unrolledMenuItem} onClick={props.onClick}>
+        Open new test…
+      </div>
+      <div className={styles.unrolledMenuItem} onClick={props.onClick}>
+        Open new test…
+      </div>
+    </Column>
+  )
+}
+
+type AppBarProps = {
+  activeApp?: string
+  setActiveApp: (value: string) => void
+  apps: App[]
+  onClick: () => void
+  appHeight?: number
+}
+const AppBar = (props: AppBarProps) => {
+  const { activeApp, setActiveApp, apps, onClick } = props
+  const [openMenu, setOpenMenu] = useState(false)
+  return (
+    <Row
+      background="var(--fff)"
+      padding="s"
+      style={{ position: 'relative' }}
+      gap="s"
+    >
+      <div
+        className={styles.menuItem}
+        style={{
+          background: openMenu ? 'var(--eee)' : undefined,
+          borderLeft: 'none',
+        }}
+        onClick={() => setOpenMenu(t => !t)}
+      >
+        Menu
+      </div>
+      {openMenu && (
+        <RenderMenu
+          {...props}
+          onClick={() => {
+            setOpenMenu(false)
+            onClick()
+          }}
+        />
+      )}
+      {openMenu && (
+        <div
+          style={{ position: 'fixed', inset: 0, zIndex: 10000 }}
+          onClick={() => setOpenMenu(false)}
+        />
+      )}
+      <Row flex={1} overflow="auto" className={styles.menuOverflow}>
+        {apps.map(app => {
+          const cl = app.id === activeApp ? styles.active : styles.app
+          return (
+            <div onClick={() => setActiveApp(app.id)} className={cl}>
+              {app.name}
+            </div>
+          )
+        })}
+      </Row>
+    </Row>
+  )
+}
+
+export type App = {
+  id: string
+  name: string
+  content: React.ReactNode
+  zIndex: number
+}
+
+export const Desktop = () => {
+  const [activeApp, setActiveApp] = useState<string>()
+  const [apps, setApps] = useState<App[]>([])
+  const [appHeight, setAppHeight] = useState<number | undefined>()
+  const appBarRef = useRef<HTMLDivElement>(null)
+  useLayoutEffect(() => {
+    if (appBarRef.current)
+      setAppHeight(appBarRef.current.getBoundingClientRect().height)
+  }, [])
+  const focusApp = (id: string) => {
+    setActiveApp(id)
+    setApps(a => {
+      return a.map(app => {
+        if (app.id === id) return { ...app, zIndex: a.length + 1 }
+        const zIndex = app.zIndex - 1
+        return { ...app, zIndex }
+      })
+    })
+  }
+  const addApp = () => {
+    setApps(a => {
+      const id = uuid.v4()
+      setActiveApp(id)
+      const content = (
+        <div style={{ maxWidth: 400, fontFamily: 'Unifont' }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      )
+      const name = 'Test'
+      const zIndex = a.length + 1
+      const app = { id, name, content, zIndex }
+      return [...a, app]
+    })
+  }
+  return (
+    <Column flex={1} overflow="hidden" background="var(--eee)">
+      <div ref={appBarRef} style={{ userSelect: 'none' }}>
+        <AppBar
+          appHeight={appHeight}
+          activeApp={activeApp}
+          setActiveApp={focusApp}
+          apps={apps}
+          onClick={addApp}
+        />
+      </div>
+      <div style={{ position: 'relative', flex: 1 }}>
+        <div className={styles.wallpaper} />
+        {apps.map(app => (
+          <Movable
+            onMouseDown={() => focusApp(app.id)}
+            title={app.name}
+            key={app.id}
+            zIndex={app.zIndex + 1000}
+            onClose={() => setApps(a => a.filter(({ id }) => app.id !== id))}
+            minWidth={300}
+            minHeight={300}
+            maxTop={appHeight}
+            padding="l"
+          >
+            {app.content}
+          </Movable>
+        ))}
+      </div>
+    </Column>
+  )
+}
+
 export const Tutorial = () => {
-  const [page, setPage] = useState<Page>('introduction')
+  const [page, setPage] = useState<Page>('start')
   switch (page) {
     case 'introduction':
       return <Introduction onNext={() => setPage('start')} />
     case 'start':
-      return null
+      return <Desktop></Desktop>
   }
 }
