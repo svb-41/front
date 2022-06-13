d0e4e1d272a2f5963fe1021811c88eaa4add9e0e
arthur.escriou <arthur.escriou@gmail.com>
2022-06-13 17:28:26 +0200

    :lipstick: format an stack

--- src/pages/account/inventory.tsx
+++ src/pages/account/inventory.tsx
@@ -5,10 +5,36 @@ import { Stuff } from '@/store/reducers/inventory'
 
 const Inventory = () => {
   const items = useSelector(selectors.inventory)
-  console.log(items.map(a => a.ownStats))
+  const stacked = items
+    .filter(item => item.type === 'metal' || item.type === 'fuel')
+    .reduce((acc: any, val: Stuff) => {
+      const type = acc[val.type]
+      if (type) {
+        if (type[val.category]) type[val.category]++
+        else type[val.category] = 1
+      } else acc[val.type] = { [val.category]: 1 }
+      return acc
+    }, {})
+  const notStacked = items.filter(
+    item => item.type !== 'metal' && item.type !== 'fuel'
+  )
+  const stackArray = Object.entries(stacked)
+    .map(([type, value]: [string, any]) =>
+      Object.entries(value)
+        .map(([category, quantity]) => ({
+          type,
+          category,
+          quantity,
+        }))
+        .flat()
+    )
+    .flat()
   return (
     <div className={styles.inventory}>
-      {items.map(s => (
+      {stackArray.map(stack => (
+        <StackedItem stack={stack} />
+      ))}
+      {notStacked.map(s => (
         <Item stuff={s} key={s.id} />
       ))}
     </div>
@@ -29,6 +55,21 @@ const color = (type: string): string => {
       return 'white'
   }
 }
+type StackItem = {
+  type: string
+  category: string
+  quantity: any
+}
+const StackedItem = ({ stack }: { stack: StackItem }) => (
+  <div className={styles.stuff}>
+    <div className={styles.type}>
+      <div style={{ color: color(stack.type) }}>{stack.type}</div>
+    </div>
+    <div className={styles.category}>{stack.category}</div>
+    {stack.type === 'piece' && <div className={styles.stats}></div>}
+    <div className={styles.stack}>X {stack.quantity}</div>
+  </div>
+)
 
 const Item = ({ stuff }: { stuff: Stuff }) => (
   <div className={styles.stuff}>
@@ -45,14 +86,23 @@ const Item = ({ stuff }: { stuff: Stuff }) => (
               key != 'distance' &&
               key != 'armed' &&
               key != 'destroyed' &&
+              key != 'builder' &&
               key != 'stats'
           )
           .map(([key, value]) => (
-            <div key={key}>{`${key}: ${value}`}</div>
+            <div key={key}>{`${key}: ${formatPi(value)}`}</div>
           ))}
       </div>
     )}
   </div>
 )
+
+const formatPi = (n: any) => {
+  if (typeof n === 'number') {
+    const size = n.toString().length > 10
+    if (size) return 'PI x ' + n / Math.PI
+  }
+  return n
+}
 
 export default Inventory
